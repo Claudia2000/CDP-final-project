f664a1d3b2245acd7f3bd7b55c958faf9269c89b
Claudia2000 <42683698+Claudia2000@users.noreply.github.com>
2021-07-06 09:01:08 -0500

    Add files via upload

---
+++ frontend/models/CDP.dart
@@ -0,0 +1,11 @@
+class CDP {
+  int idp;
+  String descripcion;
+  String nombre;
+
+  CDP(descripcion, nombre, idp) {
+    this.idp = idp;
+    this.descripcion = descripcion;
+    this.nombre = nombre;
+  }
+}
